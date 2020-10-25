34dfd6fdf95c74f22943ef93d29aa99e54e04f89
Steffen Rogge <26057594+dr0pd34d@users.noreply.github.com>
2020-10-25 11:13:54 +0100

    Added the hello world challenge

---
+++ hello-world/main.go
@@ -0,0 +1,7 @@
+//Package greeting implements a simple string return of a greeting line.
+package greeting
+
+//HelloWorld returns the string "Hello, World!"
+func HelloWorld() string {
+	return "Hello, World!"
+}
